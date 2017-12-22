cfd95d53c416ac8d299d1bdb79c86b3651f3dff4
YeHoonJang <jangyh0420@gmail.com>
2017-12-23 05:59:35 +0900

    linked hashtags

--- components/HashtagBadge.vue
+++ components/HashtagBadge.vue
@@ -1,17 +1,19 @@
 <template>
   <v-badge overlap right color="red">
     <span slot="badge">{{ cnt }}</span>
-    <v-chip small color="red" text-color="white">
-      {{ tagName.slice(1) }}
-    </v-chip>
+    <a :href="'/'+tagName.slice(1)">
+      <v-chip small color="blue" text-color="white" style="text-decoration:none">
+        {{ tagName.slice(1) }}
+      </v-chip>
+    </a>
   </v-badge>
 </template>
 
 <script>
 
-export default {
-  props: ['tagName', 'cnt']
-}
+  export default {
+    props: ['tagName', 'cnt']
+  }
 </script>
 
 <style lang="sass" scoped>
