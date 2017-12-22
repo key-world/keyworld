80e9c010fd28eb5426a341006ea85971e8e28738
YeHoonJang <jangyh0420@gmail.com>
2017-12-23 04:17:45 +0900

    hashtag color test

--- components/HashtagBadge.vue
+++ components/HashtagBadge.vue
@@ -1,7 +1,7 @@
 <template>
   <v-badge overlap right color="red">
     <span slot="badge">{{ cnt }}</span>
-    <v-btn small color="blue" text-color="white">
+    <v-btn small color="red" text-color="white">
       {{ tagName.slice(1) }}
     </v-btn>
   </v-badge>
